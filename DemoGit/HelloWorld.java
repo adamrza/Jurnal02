31c7bdd9ca10407f4c40f0afa3f74daf25768b6a
adamrza <adamlabibmrza@gmail.com>
2021-09-30 13:35:16 +0700

    HelloWorld.java

---
+++ DemoGit/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Nama	: Adam Labib Mirza");
+		System.out.println("NIM	: 6706213124");
+		System.out.println("Kelas	: 45-05");
+		System.out.println("IPK	: 4.00");
+	}
+}	
